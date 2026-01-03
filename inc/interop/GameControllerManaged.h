2c3bae2c5f9bd5c7b8477c5798cd7b992b5e477f
mochikintyaku <kt58841030@gmail.com>
2026-01-03 15:03:48 +0900

    Interop.vcxprojのひな型作製 - 現時点でビルドが通ることを確認済

---
+++ inc/interop/GameControllerManaged.h
@@ -0,0 +1,10 @@
+namespace shoryu::interop
+{
+	public value struct ManagedPosition
+	{
+		int Suji;
+		int Dan;
+
+		ManagedPosition(int suji, int dan);
+	};
+}
---
+++ src/interop/GameControllerManaged.cpp
@@ -0,0 +1,10 @@
+#include "interop/GameControllerManaged.h"
+
+
+namespace shoryu::interop
+{
+	ManagedPosition::ManagedPosition(int suji, int dan)
+		: Suji(suji), Dan(dan)
+	{
+	}
+}
